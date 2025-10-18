65075baa8624b5e3c6198731ed2008ff9f65c0ea
Frank Yang <clazex.y@gmail.com>
2025-10-18 21:31:07 +0800

    chore: support unload

--- MoreSaves/MoreSavesPlugin.cs
+++ MoreSaves/MoreSavesPlugin.cs
@@ -20,6 +20,8 @@ public partial class MoreSavesPlugin : BaseUnityPlugin {
 		private set;
 	}
 
+	private static Harmony Harmony { get; } = new(Id);
+
 	internal static event Action<ConfigEntryBase?>? ConfigChanged;
 	private static void InvokeConfigChanged(ConfigEntryBase? entry) {
 		if (entry != null) {
@@ -38,8 +40,16 @@ public partial class MoreSavesPlugin : BaseUnityPlugin {
 		ConfigEntries.Bind(Config);
 		Config.ConfigReloaded += (_, _) => InvokeConfigChanged(null);
 		Config.SettingChanged += (_, args) => InvokeConfigChanged(args.ChangedSetting);
-		Harmony.CreateAndPatchAll(typeof(Patches), Id);
+		Harmony.PatchAll(typeof(Patches));
 
 		Logger.LogInfo($"Plugin {Name} ({Id}) v{Version} has loaded!");
+	}
+
+	private void OnDestroy() {
+#if !DEBUG
+		Logger.LogWarning("Unload called in release build");
+#endif
+		Harmony.UnpatchSelf();
+		Logger.LogInfo($"Plugin {Name} has unloaded!");
 	}
 }
