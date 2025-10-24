bd776db54205c70b87082bcfe648b6004e54d008
Frank Yang <clazex.y@gmail.com>
2025-10-24 10:29:45 +0800

    feat: save clipboard

---
+++ MoreSaves/CopySaveController.cs
@@ -0,0 +1,90 @@
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+using static MoreSaves.SaveClipboard;
+
+namespace MoreSaves;
+
+[RequireComponent(typeof(MenuButton))]
+internal sealed class CopySaveController: MonoBehaviour {
+	internal static CopySaveController Instance {
+		get => field != null ? field : throw new InvalidOperationException("instance not present");
+		private set => field = value;
+	}
+
+	internal static void Setup(UIManager ui) {
+		GameObject control = ui.saveProfileControls.gameObject;
+		GameObject buttonBackGo = control.transform.Find("BackButton").gameObject;
+		MenuButton buttonBack = buttonBackGo.GetComponent<MenuButton>();
+
+		GameObject buttonCopyGo = Instantiate(buttonBackGo, control.transform);
+		buttonCopyGo.name = "CopySaveButton";
+		buttonCopyGo.transform.Translate(0f, -1.25f, 0f);
+		DestroyImmediate(buttonCopyGo.GetComponent<EventTrigger>());
+		buttonCopyGo.AddComponent<CopySaveController>();
+
+		MenuButton buttonCopy = buttonCopyGo.GetComponent<MenuButton>();
+		buttonCopy.buttonType = MenuButton.MenuButtonType.Activate; // Don't force deselect
+
+		buttonBack.navigation = buttonBack.navigation with {
+			selectOnDown = buttonCopy
+		};
+		buttonCopy.navigation = buttonCopy.navigation with {
+			selectOnUp = buttonBack
+		};
+	}
+
+	internal static void OnStateChanged(ClipboardState state) {
+		SetSaveButtonsDeselect(state == ClipboardState.Idle);
+		Instance.RefreshText();
+	}
+
+	private static void SetSaveButtonsDeselect(bool doDeselect) {
+		MenuButton.MenuButtonType type = doDeselect
+			? MenuButton.MenuButtonType.Proceed
+			: MenuButton.MenuButtonType.Activate;
+		CoreLoop.InvokeNext(() => {
+			foreach (SaveSlotButton button in SavePageState.buttons) {
+				button.buttonType = type;
+			}
+		});
+	}
+
+	private static string GetText() => State switch {
+		ClipboardState.Idle => Lang.Get("CopyIdle"),
+		ClipboardState.CopySelect => Lang.Get("CopySelect"),
+		ClipboardState.PasteSelect => string.Format(Lang.Get("CopySelected"), SaveName),
+		_ => throw new InvalidOperationException()
+	};
+
+
+	private GameObject textChild;
+	private Text text;
+	private FixVerticalAlign textAligner;
+
+	private void Awake() {
+		Instance = this;
+
+		MenuButton button = GetComponent<MenuButton>();
+		button.cancelAction = GlobalEnums.CancelAction.GoToMainMenu;
+		UnityEvent @event = new();
+		@event.AddListener(new(StartCopy));
+		button.OnSubmitPressed = @event;
+
+		textChild = transform.Find("Menu Button Text").gameObject;
+		text = textChild.GetComponent<Text>();
+		textAligner = textChild.GetComponent<FixVerticalAlign>();
+		DestroyImmediate(textChild.GetComponent<AutoLocalizeTextUI>());
+		GameManager.instance.RefreshLanguageText += RefreshText;
+		RefreshText();
+	}
+
+	private void OnDestroy() =>
+		GameManager.instance.RefreshLanguageText -= RefreshText;
+
+	private void RefreshText() {
+		text.text = GetText();
+		textAligner.AlignText();
+	}
+}
---
+++ MoreSaves/SaveClipboard.cs
@@ -0,0 +1,115 @@
+using UnityEngine.UI;
+
+namespace MoreSaves;
+
+internal static class SaveClipboard {
+	internal enum ClipboardState {
+		Idle,
+		CopySelect,
+		PasteSelect
+	}
+
+	internal static ClipboardState State {
+		get;
+		private set {
+			field = value;
+			CopySaveController.OnStateChanged(value);
+		}
+	} = ClipboardState.Idle;
+
+	private static int index = 0;
+	private static AutoSaveName name = AutoSaveName.NONE;
+	private static SaveGameData? data = null;
+
+	internal static string SaveName => name == AutoSaveName.NONE
+		? string.Format(Lang.Get("SaveIndex"), index)
+		: string.Format(Lang.Get("SaveIndexWithName"), index, GameManager.GetFormattedAutoSaveNameString(name));
+
+	internal static void StartCopy() {
+		if (State == ClipboardState.Idle) {
+			State = ClipboardState.CopySelect;
+			Plugin.Logger.LogDebug("Copy started");
+		} else {
+			CancelCopy();
+		}
+	}
+
+	internal static void CancelCopy() {
+		if (State == ClipboardState.Idle) {
+			Plugin.Logger.LogDebug("Not copying, remain idle");
+			return;
+		}
+
+		index = 0;
+		data = null;
+		State = ClipboardState.Idle;
+		Plugin.Logger.LogDebug("Copy cancelled");
+	}
+
+	/// <returns>Continues executing original function</returns>
+	internal static bool Select(int slot, SaveGameData? saveData, AutoSaveName autoSaveName = AutoSaveName.NONE) {
+		if (State == ClipboardState.Idle) {
+			Plugin.Logger.LogError("Selection occurred when idle");
+			return true;
+		}
+
+		if (saveData == null) {
+			if (State == ClipboardState.PasteSelect) {
+				PasteSave(slot);
+			} else {
+				CancelCopy();
+			}
+		} else {
+			if (State == ClipboardState.CopySelect) {
+				CopySave(slot, saveData, autoSaveName);
+			} else {
+				CancelCopy();
+			}
+		}
+
+		return false;
+	}
+
+	private static void CopySave(int slot, SaveGameData saveGameData, AutoSaveName autoSaveName) {
+		if (State != ClipboardState.CopySelect) {
+			Plugin.Logger.LogError($"State {State} is invalid to copy save");
+			CancelCopy();
+			return;
+		}
+
+		index = slot;
+		name = autoSaveName;
+		data = saveGameData;
+		State = ClipboardState.PasteSelect;
+		Plugin.Logger.LogDebug($"Copied save {SaveName}");
+	}
+
+	private static void PasteSave(int slot) {
+		Plugin.Logger.LogDebug("Start pasting");
+		CopySaveController.Instance.StartCoroutine(PasteSaveCoro(slot));
+	}
+
+	private static IEnumerator PasteSaveCoro(int slot) {
+		GameManager gm = GameManager.instance;
+		InputHandler ih = InputHandler.Instance;
+		ih.StopUIInput();
+
+		bool? result = null;
+		Platform.Current.WriteSaveSlot(slot, gm.GetBytesForSaveData(data), (success) => result = success);
+		yield return new WaitUntil(() => result.HasValue);
+
+		SaveSlotButton button = SavePageState.GetButtonForSlot(slot);
+		button.ResetButton(gm, false);
+		yield return SavePageState.loadSaveWait;
+		button.Prepare(gm);
+
+		if (result!.Value) {
+			State = ClipboardState.Idle;
+			Plugin.Logger.LogDebug($"Pasted save into {slot}");
+		} else {
+			Plugin.Logger.LogError($"Failed to paste save into {slot}");
+		}
+
+		ih.StartUIInput();
+	}
+}
